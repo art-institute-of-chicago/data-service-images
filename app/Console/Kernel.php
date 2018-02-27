670aff82cdf1234f256be528dcde051b0801015b
Illya Moskvin <imoskvin@artic.edu>
2018-02-27 13:52:50 -0600

    Add initial boilerplate
    This code was extracted from our existing dataservices, primarily
    from `data-service-catalogues`. There's one piece missing: seeders.
    Implementing them properly requires updates to the foundation.
    We should also consider upgrading to Laravel 5.6.

---
+++ app/Console/Kernel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     * Use this to import third-party commands.
+     *
+     * @var array
+     */
+    protected $commands = [
+        // \Aic\Hub\Foundation\Commands\Foobar::class
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        //
+    }
+
+    /**
+     * Register the Closure based commands for the application.
+     * By default, it loads all commands in `Commands` non-recursively.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+
+        $this->load(__DIR__.'/Commands');
+
+    }
+}
