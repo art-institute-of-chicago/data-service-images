5cc9e3c6aed877654b5fe6a2bb2b673dcd6d2d3a
Illya Moskvin <imoskvin@artic.edu>
2018-03-07 16:57:18 -0600

    Add validateId() method to Image model
    Ported from data-aggregator's Asset model.

--- app/Image.php
+++ app/Image.php
@@ -11,4 +11,20 @@ class Image extends BaseModel
         'color' => 'object',
     ];
 
+    /**
+     * Ensure that the id is a valid UUID.
+     *
+     * @param mixed $id
+     * @return boolean
+     */
+    public static function validateId($id)
+    {
+
+        // We must not be using UUIDv3, since the typical regex wasn't matching
+        $uuid = '/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i';
+
+        return preg_match($uuid, $id);
+
+    }
+
 }
