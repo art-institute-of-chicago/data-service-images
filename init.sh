49cb3154603e4a30484cc4145c8b2ea2d0903588
Illya Moskvin <imoskvin@artic.edu>
2019-01-28 05:28:42 +0000

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+DIR_ROOT="$(dirname "${BASH_SOURCE[0]}")"
+
+function warn() {
+    echo -e "\e[31m$1\e[0m"
+}
+
+if [ ! -f "$DIR_ROOT/.env" ]; then
+    warn '.env file not found. Run this to create it:'
+    echo
+    echo "    cp $DIR_ROOT/.env.example $DIR_ROOT/.env"
+    echo
+    warn 'Be sure to adjust `DB_*` values in .env before continuing!'
+    exit 1
+fi
+
+# Install dependencies
+composer install -d "$DIR_ROOT"
+
+# Initialize database
+php "$DIR_ROOT/artisan" migrate --step
+
+# Download image metadata from data-aggregator
+php "$DIR_ROOT/artisan" images:import
+
+# Download actual image files (~20 GB)
+php "$DIR_ROOT/artisan" images:download
+
+# Download info.json files from IIIF
+php "$DIR_ROOT/artisan" images:json-download
+
+# Imports info.json files into database
+php "$DIR_ROOT/artisan" images:json-import
+
+# Calculate dominant color for images
+php "$DIR_ROOT/artisan" images:color
+
+# Calculate low-quality image placeholders (LQIPs)
+php "$DIR_ROOT/artisan" images:lqip
